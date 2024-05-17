c22fb69ba9a1c02f01cc15d096333ed63f2bd0bb
Brissot <sidsu7@gmail.com>
2024-05-17 17:16:14 -0400

    CMSC335 Project Added; Repo and Code Refactoring
    Content Change: Added the CMSC335 project to the website, along with the video.
    we made at McKeldin.
    
    Made a commitment to a static personal website, so removed the Dockerfile and
    docker-compose.yml. If I want to add dynamic functionality in the future, in
    would have to be through calling another server with client-side javascript.
    
    Additionally, I split out the javascript from the website so that it can be used
    in multple files.
    
    Finally, I made new media and docs folder, separate and organize the website
    code, from the fonts, images, videos, papers and other documents.

---
+++ functions.js
@@ -0,0 +1,15 @@
+function revealEmail() {
+    /* The e-mail is reversed and encoded in Base64. This function undoes this
+    operation. This is sufficient to avoid internet scrapers, because for
+    their own sakes, they do not generally run arbritrary JavaScript code */
+    
+    const c= "==QbvNmLslWYtdGQ3U3ckl2c" /* cyphertext */
+    const m= atob(Array.from(c).reduce(((e, t) => t + e), "")); /* plaintext */
+
+    const t= document.createElement("a")
+    t.href= "mailto:" + m /* construct url */
+    t.textContent= m
+
+    document.getElementById("e-mail")?.parentElement?.appendChild(t)
+    document.getElementById("e-mail")?.remove() /* switch-a-roo! */
+}
