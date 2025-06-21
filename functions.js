8babcdba4a9bf93d3a4033d3249eefaa27cd699b
Brissot <sidsu7@gmail.com>
2025-06-20 22:22:06 -0400

    Added Live Music Page, General Restructuring

--- functions.js
+++ functions.js
@@ -1,12 +1,15 @@
+"use strict";
+
+/**
+ * The e-mail is reversed and encoded in Base64. revealEmail() undoes this
+ * operation. This is sufficient to avoid internet scrapers, because for
+ * their own sakes, they do not generally run arbritrary JavaScript code...
+ */
 function revealEmail() {
-    /* The e-mail is reversed and encoded in Base64. This function undoes this
-    operation. This is sufficient to avoid internet scrapers, because for
-    their own sakes, they do not generally run arbritrary JavaScript code */
-    
-    const c= "==QbvNmLslWYtdGQ3U3ckl2c" /* cyphertext */
+    const c= "==QbvNmLslWYtdGQ3U3ckl2c"; /* ciphertext */
     const m= atob(Array.from(c).reduce(((e, t) => t + e), "")); /* plaintext */
 
-    const t= document.createElement("a")
+    const t= document.createElement("a");
     t.href= "mailto:" + m; /* construct url */
     t.textContent= m;
 
